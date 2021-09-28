56ca55893a2edf81ff6e198bae18e818d5d61b20
dolphin.liu <dolphin.liu@cloudwise.com>
2021-09-28 16:16:44 +0800

    配置webpack别名

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Button } from 'antd-mobile'
+import Hello from '@/component/Hello';
 function App() {
   return (
     <div className="App">
       hello react
       <Button>hi</Button>
+      <Hello />
     </div>
   );
 }
